f829d7b25a95d2eec2fe2a2fd3751b54a3e5b50f
Miguel Veloso <mvelosop@gmail.com>
2024-03-30 12:08:13 +0000

    buy more books 😉

--- code/merge-and-revert/app.ts
+++ code/merge-and-revert/app.ts
@@ -6,11 +6,14 @@ const myLibrary = new Library();
 
 myLibrary.addBook(new Book("The Hobbit", "J.R.R. Tolkien", 1937));
 myLibrary.addBook(new Book("1984", "George Orwell", 1949));
-myLibrary.addBook(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997));
 myLibrary.addBook(new Book("Foundation", "Isaac Asimov", 1951));
+myLibrary.addBook(new Book("The End of Eternity", "Isaac Asimov", 1955));
+myLibrary.addBook(new Book("Fahrenheit 451", "Ray Bradbury", 1953));
+myLibrary.addBook(new Book("2001: A Space Odyssey", "Arthur C. Clarke", 1968));
+myLibrary.addBook(new Book("Dune", "Frank Herbert", 1965));
 
 console.log(`
-Comprehensive Library Catalog
-=============================
+Library catalog
+===============
 `);
 myLibrary.listBooks();
